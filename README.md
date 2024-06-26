c4a886655b9b08c7d63fd0717b19a456789d8c6d
Shira Saban Biton <you@example.com>
2024-06-26 17:29:32 +0300

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ex5
+
+http://localhost:3000/profile?id=mickey for get mickey profile
+
+Shira Saban Bitton - 316511658
+Fida Rabah - 204647911
+25/6/24
--- profile.js
+++ profile.js
@@ -1,6 +1,6 @@
 // Shira Saban Bitton - 316511658
 // Fida Rabah - 204647911
-// 12/3/24
+// 25/6/24
 
 // This server script uses Express to serve a profile page.
 // It reads profile data from the file system and provides it to the client upon request.
@@ -17,45 +17,54 @@ app.use(express.static("public"));
 
 // Serve the profile HTML page
 app.get("/profile", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "profile.html"));
+  res.sendFile(path.join(__dirname, "public", "profile.html"));
 });
 
 // Provide profile data in JSON format
 app.get("/profile-data", (req, res) => {
-    let id = req.query.id;
-    let profilePath = path.join(__dirname, "private", id);
+  let id = req.query.id;
+  let profilePath = path.join(__dirname, "private", id);
 
-    fs.readdir(profilePath, (err, files) => {
-        if (err) {
-            console.error("Error reading directory:", err);
-            return res.status(500).send("Error reading profile directory");
-        }
+  fs.readdir(profilePath, (err, files) => {
+    if (err) {
+      console.error("Error reading directory:", err);
+      return res.status(500).send("Error reading profile directory");
+    }
 
-        const textFiles = files.filter(file => file.startsWith("text") && file.endsWith(".txt"));
-        let contentFiles = textFiles.map(file => fs.readFileSync(path.join(profilePath, file), "utf8"));
+    const textFiles = files.filter(
+      (file) => file.startsWith("text") && file.endsWith(".txt")
+    );
+    let contentFiles = textFiles.map((file) =>
+      fs.readFileSync(path.join(profilePath, file), "utf8")
+    );
 
-        let bioText = fs.readFileSync(path.join(profilePath, "bio.txt"), "utf8");
-        const htmlBioContent = bioText.split(":");
+    let bioText = fs.readFileSync(path.join(profilePath, "bio.txt"), "utf8");
+    const htmlBioContent = bioText.split(":");
 
-        let fullTitleText = fs.readFileSync(path.join(profilePath, "title.txt"), "utf8");
-        const [titleText, descText] = fullTitleText.split("\r\n");
+    let fullTitleText = fs.readFileSync(
+      path.join(profilePath, "title.txt"),
+      "utf8"
+    );
+    const [titleText, descText] = fullTitleText.split("\r\n");
 
-        const privateFolder = path.join(__dirname, "private");
-        const filesInPri = fs.readdirSync(privateFolder);
-        const profilesNames = filesInPri.filter(file => fs.statSync(path.join(privateFolder, file)).isDirectory());
+    const privateFolder = path.join(__dirname, "private");
+    const filesInPri = fs.readdirSync(privateFolder);
+    const profilesNames = filesInPri.filter((file) =>
+      fs.statSync(path.join(privateFolder, file)).isDirectory()
+    );
 
-        res.json({
-            id,
-            descText,
-            titleText,
-            htmlBioContent,
-            contentFiles,
-            profilesNames
-        });
+    res.json({
+      id,
+      descText,
+      titleText,
+      htmlBioContent,
+      contentFiles,
+      profilesNames,
     });
+  });
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
